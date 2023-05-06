666f3d9112ee5a09c0032d40161278a6ec289e42
Sang Dang Yeu <sangtranbao7@gmail.com>
2023-05-06 21:22:16 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import React from 'react';
+// import './App.css';
+import { increaseCounter, decreaseCounter } from './redux/action/actions';
+import Home from './components/Home';
+const App = (props) => {
+  return (
+    <Home />
+  )
+}
+
+
+
+export default App;
+
