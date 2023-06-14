29b8cdb8fc6f712b94c82a998b278d68e3137561
Sang Dang Yeu <sangtranbao7@gmail.com>
2023-06-14 21:24:01 +0700

    todo app

--- src/index.js
+++ src/index.js
@@ -3,19 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from './redux/store';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { createStore } from 'redux';
+import rootReducer from './store/reducer/rootReducer';
+
+const reduxStore = createStore(rootReducer);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
+  <Provider store={reduxStore}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Provider>,
-
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
